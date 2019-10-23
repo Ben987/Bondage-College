78e1a7af29970b5743c1e14b81e56207790ff2d9
EveFerox <eveferox@gmail.com>
2019-10-23 18:26:20 +0300

    change OnlineProfile code from class to functions

--- BondageClub/Screens/Character/OnlineProfile/OnlineProfile.js
+++ BondageClub/Screens/Character/OnlineProfile/OnlineProfile.js
@@ -1,140 +1,95 @@
 "use strict";
 
-class OnlineProfile {
-    IsDebug = false;
-    CurrentChar = null;
-    IsEditMode = false;
-    
-    DescriptionInputMaxLength = 1000;
-    DescriptionInputRect = [100,200,1650,750];
-
-    constructor() {
-        this.CurrentChar = null;
-    }
-
-    SetEditMode(isEdit) {
-        this.IsEditMode = isEdit;
-
-        var C = this.CurrentChar;
-
-        ElementRemove("DescriptionInput");
-        ElementCreateTextArea("DescriptionInput");
-
-        var DescriptionInput = document.getElementById("DescriptionInput");
-        DescriptionInput.setAttribute("maxlength", this.DescriptionInputMaxLength);
-        DescriptionInput.value = C.Description;
-
-        if (!isEdit) {
-            DescriptionInput.setAttribute("readonly", "readonly");
-        }
-
-        this.Log("SetEditMode = " + this.IsEditMode);
-    }
-
-    ToggleEditMode() {
-        this.Log("ToggleEditMode");
-        this.SetEditMode(!this.IsEditMode);
-    }
-
-    Load(C) {
-        this.CurrentChar = C;
-        this.SetEditMode(false);
-    }
-
-    Run() {
-        var C = this.CurrentChar;
-
-        MainCanvas.textAlign = "center";
-        
-        // Header text
-        DrawText("- Online Profile -", 1000, 100, "Black", "Gray");
-        
-        MainCanvas.textAlign = "left";
-
-        var desc = ElementValue("DescriptionInput");
-        DrawText("Description: ("+ desc.length + "/" + this.DescriptionInputMaxLength + " characters)", 100, 150, "Black", "Gray");
-        var P = this.DescriptionInputRect;
-        ElementPositionFix("DescriptionInput", 36, P[0], P[1], P[2], P[3]);
-
-        MainCanvas.textAlign = "center";
-
-        // Buttons
-        DrawButton(1815, 75, 90, 90, "", "White", "Icons/Exit.png", "Back");
-        if (C.ID == 0) {
-            if (this.IsEditMode) {
-                DrawButton(1815, 420, 90, 90, "", "White", "Icons/Cancel.png", "Cancel");
-                DrawButton(1815, 535, 90, 90, "", "White", "Icons/ColorSelect.png", "Save Changes");
-            } else {
-                DrawButton(1815, 420, 90, 90, "", "White", "Icons/Use.png", "Edit");
-            }
-        }
-    }
-
-    Click() {
-        var C = this.CurrentChar;
-        if (CommonIsClickAt(1815, 75, 90, 90)) this.Exit();
-        if ((C.ID == 0)) {
-            if (this.IsEditMode) {
-                if (CommonIsClickAt(1815, 420, 90, 90)) this.ToggleEditMode();
-                if (CommonIsClickAt(1815, 535, 90, 90)) this.Save();
-            } else {
-                if (CommonIsClickAt(1815, 420, 90, 90)) this.ToggleEditMode();
-            }
-        }
-    }
-
-    Exit() {
-        this.Log("Exit");
-        
-        ElementRemove("DescriptionInput");
-        
-        CommonSetScreen("Character", "InformationSheet");
-    }
-
-    Save() {
-        this.Log("Save");
-        var C = this.CurrentChar;
-
-        var isChanged = false;
-
-        var desc = ElementValue("DescriptionInput").trim();
-        if (C.Description != desc) {
-            C.Description = desc;
-            isChanged = true;
-        }
-
-        if (isChanged) {
-            ServerSend("AccountUpdate",
-                {
-                    Description: C.Description
-                });
-            ChatRoomCharacterUpdate(C);
-        }
-
-        this.SetEditMode(false);
-    }
-
-    Log(msg) {
-        if (this.IsDebug == false) return;
-        console.log(msg);
-    }
-}
-
 var OnlineProfileBackground = "Sheet";
-var OnlineProfileScreen = new OnlineProfile();
+var OnlineProfileIsEditMode = false;
+var OnlineProfileDescriptionInputMaxLength = 1000;
 
 function OnlineProfileLoad() {
-    OnlineProfileScreen.Load(InformationSheetSelection);
+    OnlineProfileSetEditMode(false);
 }
 
 function OnlineProfileRun() {
-    OnlineProfileScreen.Run();
+    var C = InformationSheetSelection;
+
+    MainCanvas.textAlign = "center";
+    
+    // Header text
+    DrawText("- Online Profile -", 1000, 100, "Black", "Gray");
+    
+    MainCanvas.textAlign = "left";
+
+    var desc = ElementValue("DescriptionInput");
+    DrawText("Description: ("+ desc.length + "/" + OnlineProfileDescriptionInputMaxLength + " characters)", 100, 150, "Black", "Gray");
+    ElementPositionFix("DescriptionInput", 36, 100,200,1650,750);
+
+    MainCanvas.textAlign = "center";
+
+    // Buttons
+    DrawButton(1815, 75, 90, 90, "", "White", "Icons/Exit.png", "Back");
+    if (C.ID == 0) {
+        if (OnlineProfileIsEditMode) {
+            DrawButton(1815, 420, 90, 90, "", "White", "Icons/Cancel.png", "Cancel");
+            DrawButton(1815, 535, 90, 90, "", "White", "Icons/ColorSelect.png", "Save Changes");
+        } else {
+            DrawButton(1815, 420, 90, 90, "", "White", "Icons/Use.png", "Edit");
+        }
+    }
 }
 
 function OnlineProfileClick() {
-    OnlineProfileScreen.Click();
+    var C = InformationSheetSelection;
+    if (CommonIsClickAt(1815, 75, 90, 90)) OnlineProfileExit();
+    if ((C.ID == 0)) {
+        if (OnlineProfileIsEditMode) {
+            if (CommonIsClickAt(1815, 420, 90, 90)) OnlineProfileSetEditMode(!OnlineProfileIsEditMode);
+            if (CommonIsClickAt(1815, 535, 90, 90)) OnlineProfileSave();
+        } else {
+            if (CommonIsClickAt(1815, 420, 90, 90)) OnlineProfileSetEditMode(!OnlineProfileIsEditMode);
+        }
+    }
 }
 
 function OnlineProfileExit() {
-    OnlineProfileScreen.Exit();
+    ElementRemove("DescriptionInput");
+    
+    CommonSetScreen("Character", "InformationSheet");
+}
+
+function OnlineProfileSetEditMode(isEdit) {
+    OnlineProfileIsEditMode = isEdit;
+
+    var C = InformationSheetSelection;
+
+    ElementRemove("DescriptionInput");
+    ElementCreateTextArea("DescriptionInput");
+
+    var DescriptionInput = document.getElementById("DescriptionInput");
+    DescriptionInput.setAttribute("maxlength", OnlineProfileDescriptionInputMaxLength);
+    DescriptionInput.value = C.Description;
+
+    if (!isEdit) {
+        DescriptionInput.setAttribute("readonly", "readonly");
+    }
+}
+
+function OnlineProfileSave(){
+    var C = InformationSheetSelection;
+
+    var isChanged = false;
+
+    var desc = ElementValue("DescriptionInput").trim();
+    if (C.Description != desc) {
+        C.Description = desc;
+        isChanged = true;
+    }
+
+    if (isChanged) {
+        ServerSend("AccountUpdate",
+            {
+                Description: C.Description
+            });
+        ChatRoomCharacterUpdate(C);
+    }
+
+    OnlineProfileSetEditMode(false);
 }
