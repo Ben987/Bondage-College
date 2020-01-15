39e7e1f79b8166c6f8bff4e2ce11c91e7c65190c
Alessio Lentini <alessiolentini@gmail.com>
2020-01-15 03:27:46 +0100

    Bugfix about checking the Effect property before drawing the custom wand button

--- BondageClub/Screens/Inventory/ItemArms/FullLatexSuit/FullLatexSuit.js
+++ BondageClub/Screens/Inventory/ItemArms/FullLatexSuit/FullLatexSuit.js
@@ -31,7 +31,13 @@ function InventoryItemArmsFullLatexSuitDraw() {
 	DrawButton(1600, 450, 225, 225, "", ((DialogFocusItem.Property.Type != null) && (DialogFocusItem.Property.Type == "UnZip")) ? "#888888" : "White");
 	DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/UnZip.png", 1600, 449);
 	DrawText(DialogFind(Player, "FullLatexSuitTypeUnZip"), 1713, 425, "white", "gray");
-	if(InventoryGet(C, "ItemVulvaPiercings") == null || InventoryGet(C, "ItemVulvaPiercings").Asset.Effect.indexOf("Chaste") == -1){
+	if(InventoryGet(C, "ItemVulvaPiercings") == null || 
+		
+		!InventoryGet(C, "ItemVulvaPiercings").Asset ||
+		!InventoryGet(C, "ItemVulvaPiercings").Asset.Effect ||
+		InventoryGet(C, "ItemVulvaPiercings").Asset.Effect.indexOf("Chaste") == -1)
+		
+	{	
 		DrawButton(1375, 750, 225, 225, "", (InventoryGet(C, "ItemVulva") == null) ? "White" : "White");
 		DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/Wand.png", 1375, 749);
 		DrawText(DialogFind(Player, "FullLatexSuitTypeWand"), 1488, 750, "white", "gray");
