ca5b47c4712565dcd3f01a2c0257b46002ae2418
Atasly <57838405+Atasly@users.noreply.github.com>
2020-05-30 19:24:09 +0200

    seamless

--- BondageClub/Scripts/Server.js
+++ BondageClub/Scripts/Server.js
@@ -530,8 +530,8 @@ function ServerAccountLovership(data) {
 		if ((CurrentCharacter != null) && (CurrentCharacter.MemberNumber == data.MemberNumber))
 			ChatRoomLovershipOption = data.Result;
 
-	// If we must update the character lovership data
-	if ((data != null) && (typeof data === "object") && !Array.isArray(data) && (data.Lovership != null) && (typeof data.Lovership === "object")) {
+	// If we must update the character lovership data as an array
+	if ((data != null) && (typeof data === "object") && !Array.isArray(data) && (data.Lovership != null) && (Array.isArray(data.Lovership))) {
 		if (data.Lovership.length > 0) {
 			Player.Lovership = data.Lovership[0];
 		}
@@ -540,6 +540,20 @@ function ServerAccountLovership(data) {
 			Player.Lovership = null;
 		}
 		LoginLoversItems();
+	}
+	else {
+		// If we must update the character lovership data
+		if ((data != null) && (typeof data === "object") && !Array.isArray(data) && (data.Lover != null) && (typeof data.Lover === "string") && (data.Lovership != null) && (typeof data.Lovership === "object")) {
+			Player.Lover = data.Lover;
+			Player.Lovership = data.Lovership;
+		}
+
+		// If we must clear the character lovership data
+		if ((data != null) && (typeof data === "object") && !Array.isArray(data) && (data.ClearLovership != null) && (typeof data.ClearLovership === "boolean") && (data.ClearLovership == true)) {
+			Player.Lover = "";
+			Player.Lovership = null;
+			LoginLoversItems();
+		}
 	}
 
 }
