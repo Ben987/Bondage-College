6ce5484609cb65738df206a011ffa5405f8f0ef3
Nina-1474 <62667047+Nina-1474@users.noreply.github.com>
2020-07-10 23:12:12 +0100

    Removing ArmsOnly properties

--- BondageClub/Screens/Inventory/ItemArms/Chains/Chains.js
+++ BondageClub/Screens/Inventory/ItemArms/Chains/Chains.js
@@ -4,60 +4,51 @@ const ChainsArmsOptions = [
 	{
 		Name: "BoxTie",
 		RequiredBondageLevel: null,
-		Property: { Type: null, Effect: ["Block", "Prone"], SetPose: ["BackBoxTie"], Difficulty: 1 },
-		ArmsOnly: true
+		Property: { Type: null, Effect: ["Block", "Prone"], SetPose: ["BackBoxTie"], Difficulty: 1 }
 	}, {
 		Name: "WristTie",
 		RequiredBondageLevel: null,
 		Property: { Type: "WristTie", Effect: ["Block", "Prone"], SetPose: ["BackBoxTie"], Difficulty: 1 },
-		Expression: [{ Group: "Blush", Name: "Low", Timer: 5 }],
-		ArmsOnly: true
+		Expression: [{ Group: "Blush", Name: "Low", Timer: 5 }]
 	}, {
 		Name: "ChainCuffs",
 		RequiredBondageLevel: null,
 		Property: { Type: "ChainCuffs", Effect: ["Block", "Prone"], SetPose: ["BackCuffs"], Difficulty: 1, OverridePriority: 30 },
-		Expression: [{ Group: "Blush", Name: "Low", Timer: 5 }],
-		ArmsOnly: true
+		Expression: [{ Group: "Blush", Name: "Low", Timer: 5 }]
 	}, {
 		Name: "WristElbowTie",
 		RequiredBondageLevel: 2,
 		Property: { Type: "WristElbowTie", Effect: ["Block", "Prone"], SetPose: ["BackElbowTouch"], Difficulty: 2 },
-		Expression: [{ Group: "Blush", Name: "Medium", Timer: 5 }],
-		ArmsOnly: true
+		Expression: [{ Group: "Blush", Name: "Medium", Timer: 5 }]
 	}, {
 		Name: "WristElbowHarnessTie",
 		RequiredBondageLevel: 3,
 		Property: { Type: "WristElbowHarnessTie", Effect: ["Block", "Prone"], SetPose: ["BackElbowTouch"], Difficulty: 3 },
-		Expression: [{ Group: "Blush", Name: "Medium", Timer: 5 }],
-		ArmsOnly: true
+		Expression: [{ Group: "Blush", Name: "Medium", Timer: 5 }]
 	}, {
 		Name: "KneelingHogtie",
 		RequiredBondageLevel: 4,
 		Prerequisite: ["NotMounted", "NotSuspended"],
 		Property: { Type: "KneelingHogtie", Effect: ["Block", "Freeze", "Prone"], Block: ["ItemHands", "ItemLegs", "ItemFeet", "ItemBoots", "ItemDevices"], SetPose: ["Kneel", "BackElbowTouch"], Difficulty: 3 },
-		Expression: [{ Group: "Blush", Name: "Medium", Timer: 10 }],
-		ArmsOnly: false
+		Expression: [{ Group: "Blush", Name: "Medium", Timer: 10 }]
 	}, {
 		Name: "Hogtied",
 		RequiredBondageLevel: 4,
 		Prerequisite: ["NotMounted", "NotSuspended", "CannotBeHogtiedWithAlphaHood"],
 		Property: { Type: "Hogtied", Effect: ["Block", "Freeze", "Prone"], Block: ["ItemHands", "ItemLegs", "ItemFeet", "ItemBoots", "ItemDevices"], SetPose: ["Hogtied"], Difficulty: 3 },
-		Expression: [{ Group: "Blush", Name: "Medium", Timer: 10 }],
-		ArmsOnly: false
+		Expression: [{ Group: "Blush", Name: "Medium", Timer: 10 }]
 	}, {
 		Name: "AllFours",
 		RequiredBondageLevel: 6,
 		Prerequisite: ["NotMounted", "NotSuspended", "CannotBeHogtiedWithAlphaHood"],
 		Property: { Type: "AllFours", Effect: ["ForceKneel"], Block: ["ItemLegs", "ItemFeet", "ItemBoots", "ItemDevices"], SetPose: ["AllFours"], Difficulty: 3 },
-		Expression: [{ Group: "Blush", Name: "Medium", Timer: 10 }],
-		ArmsOnly: false
+		Expression: [{ Group: "Blush", Name: "Medium", Timer: 10 }]
 	}, {
 		Name: "SuspensionHogtied",
 		RequiredBondageLevel: 8,
 		Prerequisite: ["NotMounted", "NotChained", "NotSuspended", "CannotBeHogtiedWithAlphaHood"],
 		Property: { Type: "SuspensionHogtied", Effect: ["Block", "Freeze", "Prone"], Block: ["ItemHands", "ItemLegs", "ItemFeet", "ItemBoots"], SetPose: ["Hogtied", "SuspensionHogtied"], Difficulty: 6 },
 		Expression: [{ Group: "Blush", Name: "Medium", Timer: 10 }],
-		ArmsOnly: false,
 		HiddenItem: "SuspensionChains"
 	}
 ];
