61869e6cdccd0597a6a10ffd57f472cddcb9cba6
Ace <>
2020-07-11 21:08:10 -0400

    Lactation Pump improvements

--- BondageClub/Screens/Inventory/ItemNipples/LactationPump/LactationPump.js
+++ BondageClub/Screens/Inventory/ItemNipples/LactationPump/LactationPump.js
@@ -11,27 +11,21 @@ function InventoryItemNipplesLactationPumpDraw() {
 	DrawRect(1387, 225, 225, 275, "white");
 	DrawImageResize("Assets/" + DialogFocusItem.Asset.Group.Family + "/" + DialogFocusItem.Asset.Group.Name + "/Preview/" + DialogFocusItem.Asset.Name + ".png", 1389, 227, 221, 221);
 	DrawTextFit(DialogFocusItem.Asset.Description, 1500, 475, 221, "black");
-	DrawText(DialogFind(Player, "SuctionPower" + DialogFocusItem.Property.SuctionLevel.toString()), 1500, 600, "White", "Gray");
-	if(DialogFocusItem.Property.SuctionLevel > 0) DrawButton(1200, 650, 200, 55, DialogFind(Player, "off"), "White");
-	if(DialogFocusItem.Property.SuctionLevel < 1) DrawButton(1550, 650, 200, 55, DialogFind(Player, "LowSuction"), "White");
-	if(DialogFocusItem.Property.SuctionLevel > 1) DrawButton(1550, 650, 200, 55, DialogFind(Player, "LowSuction"), "White");
-	if(DialogFocusItem.Property.SuctionLevel < 2) DrawButton(1200, 710, 200, 55, DialogFind(Player, "MediumSuction"), "White");
-	if(DialogFocusItem.Property.SuctionLevel > 2) DrawButton(1200, 710, 200, 55, DialogFind(Player, "MediumSuction"), "White");
-	if(DialogFocusItem.Property.SuctionLevel < 3) DrawButton(1550, 710, 200, 55, DialogFind(Player, "HighSuction"), "White");
-	if(DialogFocusItem.Property.SuctionLevel > 3) DrawButton(1550, 710, 200, 55, DialogFind(Player, "HighSuction"), "White");
-	if(DialogFocusItem.Property.SuctionLevel < 4) DrawButton(1375, 770, 200, 55, DialogFind(Player, "MaximumSuction"), "White");
+	DrawText(DialogFind(Player, "LactationPumpSuctionPower" + DialogFocusItem.Property.SuctionLevel.toString()), 1500, 600, "White", "Gray");
+	if(DialogFocusItem.Property.SuctionLevel > 0) DrawButton(1200, 650, 200, 55, DialogFind(Player, "LactationPumpoff"), "White");
+	if(DialogFocusItem.Property.SuctionLevel != 1) DrawButton(1550, 650, 200, 55, DialogFind(Player, "LactationPumpLowSuction"), "White");
+	if(DialogFocusItem.Property.SuctionLevel != 2) DrawButton(1200, 710, 200, 55, DialogFind(Player, "LactationPumpMediumSuction"), "White");
+	if(DialogFocusItem.Property.SuctionLevel != 3) DrawButton(1550, 710, 200, 55, DialogFind(Player, "LactationPumpHighSuction"), "White");
+	if(DialogFocusItem.Property.SuctionLevel < 4) DrawButton(1375, 770, 200, 55, DialogFind(Player, "LactationPumpMaximumSuction"), "White");
 }
 
 // Catches the item extension clicks
 function InventoryItemNipplesLactationPumpClick() {
 	if ((MouseX >= 1885) && (MouseX <= 1975) && (MouseY >= 25) && (MouseY <= 110)) DialogFocusItem = null;
 	if ((MouseX >= 1200) && (MouseX <= 1400) && (MouseY >= 650) && (MouseY <= 705) && (DialogFocusItem.Property.SuctionLevel > 0)) InventoryItemNipplesLactationPumpIntensity(0 - DialogFocusItem.Property.SuctionLevel);
-	if ((MouseX >= 1550) && (MouseX <= 1750) && (MouseY >= 650) && (MouseY <= 705) && (DialogFocusItem.Property.SuctionLevel < 1)) InventoryItemNipplesLactationPumpIntensity(1 - DialogFocusItem.Property.SuctionLevel);
-	if ((MouseX >= 1550) && (MouseX <= 1750) && (MouseY >= 650) && (MouseY <= 705) && (DialogFocusItem.Property.SuctionLevel > 1)) InventoryItemNipplesLactationPumpIntensity(1 - DialogFocusItem.Property.SuctionLevel);
-	if ((MouseX >= 1200) && (MouseX <= 1400) && (MouseY >= 710) && (MouseY <= 765) && (DialogFocusItem.Property.SuctionLevel < 2)) InventoryItemNipplesLactationPumpIntensity(2 - DialogFocusItem.Property.SuctionLevel);
-	if ((MouseX >= 1200) && (MouseX <= 1400) && (MouseY >= 710) && (MouseY <= 765) && (DialogFocusItem.Property.SuctionLevel > 2)) InventoryItemNipplesLactationPumpIntensity(2 - DialogFocusItem.Property.SuctionLevel);
-	if ((MouseX >= 1550) && (MouseX <= 1750) && (MouseY >= 710) && (MouseY <= 765) && (DialogFocusItem.Property.SuctionLevel > 3)) InventoryItemNipplesLactationPumpIntensity(3 - DialogFocusItem.Property.SuctionLevel);
-	if ((MouseX >= 1550) && (MouseX <= 1750) && (MouseY >= 710) && (MouseY <= 765) && (DialogFocusItem.Property.SuctionLevel < 3)) InventoryItemNipplesLactationPumpIntensity(3 - DialogFocusItem.Property.SuctionLevel);
+	if ((MouseX >= 1550) && (MouseX <= 1750) && (MouseY >= 650) && (MouseY <= 705) && (DialogFocusItem.Property.SuctionLevel != 1)) InventoryItemNipplesLactationPumpIntensity(1 - DialogFocusItem.Property.SuctionLevel);
+	if ((MouseX >= 1200) && (MouseX <= 1400) && (MouseY >= 710) && (MouseY <= 765) && (DialogFocusItem.Property.SuctionLevel != 2)) InventoryItemNipplesLactationPumpIntensity(2 - DialogFocusItem.Property.SuctionLevel);
+	if ((MouseX >= 1550) && (MouseX <= 1750) && (MouseY >= 710) && (MouseY <= 765) && (DialogFocusItem.Property.SuctionLevel != 3)) InventoryItemNipplesLactationPumpIntensity(3 - DialogFocusItem.Property.SuctionLevel);
 	if ((MouseX >= 1375) && (MouseX <= 1575) && (MouseY >= 770) && (MouseY <= 825) && (DialogFocusItem.Property.SuctionLevel < 4)) InventoryItemNipplesLactationPumpIntensity(4 - DialogFocusItem.Property.SuctionLevel);
 }
 
