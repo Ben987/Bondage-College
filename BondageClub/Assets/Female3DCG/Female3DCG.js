c31a54bf188a1a723af4569781b50a628d8e8186
Ace <>
2020-07-24 18:54:41 -0400

    Changed

--- BondageClub/Assets/Female3DCG/Female3DCG.js
+++ BondageClub/Assets/Female3DCG/Female3DCG.js
@@ -29,6 +29,17 @@ var AssetSpankingToys = {
 	Effect: []
 };
 
+// Eyes group
+var AssetEyes = {
+	Priority: 9,
+	Blink: true,
+	FullAlpha: false,
+	AllowNone: false,
+	AllowExpression: ["Closed", "Dazed", "Shy", "Sad", "Horny", "Lewd", "VeryLewd", "Heart", "HeartPink", "LewdHeart", "LewdHeartPink", "Dizzy", "Daydream", "Angry", "Surprised", "Scared", "Wink"],
+	Asset: ["Eyes1", "Eyes2", "Eyes3", "Eyes4", "Eyes5", "Eyes6", "Eyes7", "Eyes8", "Eyes9", "Eyes10", "Eyes11"],
+	Color: ["#6a3628", "#5e481e", "#666666", "#555588", "#558855", "#885555", "#202020", "#aa3333", "#33aa33", "#3333aa", "#aaaa33", "#33aaaa", "#aa33aa"]
+}
+
 // 3D Custom Girl based assets
 var AssetFemale3DCG = [
 
@@ -789,33 +800,19 @@ var AssetFemale3DCG = [
 		Color: ["#6a3628", "#202020", "#dcc787", "#6c2132", "#999999", "#dddddd", "#e781b1", "#81e7b1", "#81b1e7", "#eeee99", "#ee9999", "#ee99ee"]
 	},
 
-	{
+	Object.assign({}, AssetEyes, {
 		Group: "Eyes",
-		Priority: 9,
-		Blink: true,
 		Left: 200,
-		Top: 145,
-		FullAlpha: false,
-		AllowNone: false,
-		MirrorGroup: "Eyes2",
-		AllowExpression: ["Closed", "Dazed", "Shy", "Sad", "Horny", "Lewd", "VeryLewd", "Heart", "HeartPink", "LewdHeart", "LewdHeartPink", "Dizzy", "Daydream", "Angry", "Surprised", "Scared", "Wink"],
-		Asset: ["Eyes1", "Eyes2", "Eyes3", "Eyes4", "Eyes5", "Eyes6", "Eyes7", "Eyes8", "Eyes9", "Eyes10", "Eyes11"],
-		Color: ["#6a3628", "#5e481e", "#666666", "#555588", "#558855", "#885555", "#202020", "#aa3333", "#33aa33", "#3333aa", "#aaaa33", "#33aaaa", "#aa33aa"]
-	},
+		Top: 145
+	}),
 
-	{
+	
+	Object.assign({}, AssetEyes, {
 		Group: "Eyes2",
-		Priority: 9,
-		Blink: true,
 		Left: 250,
 		Top: 145,
-		FullAlpha: false,
-		AllowNone: false,
 		MirrorGroup: "Eyes",
-		AllowExpression: ["Closed", "Dazed", "Shy", "Sad", "Horny", "Lewd", "VeryLewd", "Heart", "HeartPink", "LewdHeart", "LewdHeartPink", "Dizzy", "Daydream", "Angry", "Surprised", "Scared", "Wink"],
-		Asset: [{ Name: "Eyes1", ParentItem: "Eyes1" }, { Name: "Eyes2", ParentItem: "Eyes2" }, { Name: "Eyes3", ParentItem: "Eyes3" }, { Name: "Eyes4", ParentItem: "Eyes4" }, { Name: "Eyes5", ParentItem: "Eyes5" }, { Name: "Eyes6", ParentItem: "Eyes6" }, { Name: "Eyes7", ParentItem: "Eyes7" }, { Name: "Eyes8", ParentItem: "Eyes8" }, { Name: "Eyes9", ParentItem: "Eyes9" }, { Name: "Eyes10", ParentItem: "Eyes10" }, { Name: "Eyes11", ParentItem: "Eyes11" }],
-		Color: ["#6a3628", "#5e481e", "#666666", "#555588", "#558855", "#885555", "#202020", "#aa3333", "#33aa33", "#3333aa", "#aaaa33", "#33aaaa", "#aa33aa"]
-	},
+	}),
 
 	{
 		Group: "Mouth",
