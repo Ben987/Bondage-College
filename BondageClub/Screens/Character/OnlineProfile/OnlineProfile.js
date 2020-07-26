73ae8469273d6fd2adc0ccf834386d61acbb914c
Ace <>
2020-07-26 18:36:44 -0400

    Save description

--- BondageClub/Screens/Character/OnlineProfile/OnlineProfile.js
+++ BondageClub/Screens/Character/OnlineProfile/OnlineProfile.js
@@ -24,7 +24,8 @@ function OnlineProfileRun() {
     var desc = ElementValue("DescriptionInput");
     DrawText(TextGet((InformationSheetSelection.ID == 0) ? "EnterDescription" : "ViewDescription").replace("CharacterName", InformationSheetSelection.Name), 910, 105, "Black", "Gray");
     ElementPositionFix("DescriptionInput", 36, 100, 160, 1790, 750);
-    DrawButton(1820, 60, 90, 90, "", "White", "Icons/Exit.png");
+    if (InformationSheetSelection.ID == 0) DrawButton(1720, 60, 90, 90, "", "White", "Icons/Accept.png", TextGet("LeaveSave"));
+    DrawButton(1820, 60, 90, 90, "", "White", ((InformationSheetSelection.ID == 0) ? "Icons/Cancel.png" : "Icons/Exit.png"), TextGet((InformationSheetSelection.ID == 0) ? "LeaveNoSave" : "Leave"));
 
 }
 
@@ -33,16 +34,18 @@ function OnlineProfileRun() {
  * @returns {void} - Nothing
  */
 function OnlineProfileClick() {	
-    if (MouseIn(1820, 60, 90, 90)) OnlineProfileExit();
+    if (MouseIn(1820, 60, 90, 90)) OnlineProfileExit(false);
+    if (InformationSheetSelection.ID == 0 && MouseIn(1720, 60, 90, 90)) OnlineProfileExit(true);
 }
 
 /**
  * Handles exiting while in the online profile screen. It removes the input and saves the description.
+ * @param {boolean} Save - Whether or not we should save the changes
  * @returns {void} - Nothing
  */
-function OnlineProfileExit() {
+function OnlineProfileExit(Save) {
     // If the current character is the player, we update the description
-    if ((InformationSheetSelection.ID == 0) && (InformationSheetSelection.Description != ElementValue("DescriptionInput").trim())) {
+    if ((InformationSheetSelection.ID == 0) && (InformationSheetSelection.Description != ElementValue("DescriptionInput").trim()) && Save) {
         InformationSheetSelection.Description = ElementValue("DescriptionInput").trim();
         ServerSend("AccountUpdate", { Description: InformationSheetSelection.Description });
         ChatRoomCharacterUpdate(InformationSheetSelection);
