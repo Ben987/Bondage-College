88adddf2b28e0c5c63b397615ec9ecf49f5c3f15
Ace <>
2020-08-14 04:19:57 -0400

    Revert "Merge branch 'Extended-script-upgrade' into extended-necklace"
    This reverts commit f0659cbd67d7006a79b9b2224f9b7e1ab7592d2e, reversing
    changes made to 5250fbc9293267226b0b4969217e11dcd8439511.

--- BondageClub/Scripts/ExtendedItem.js
+++ BondageClub/Scripts/ExtendedItem.js
@@ -103,10 +103,9 @@ function ExtendedItemDraw(Options, DialogPrefix) {
  * Handles clicks on the extended item type selection screen
  * @param {ExtendedItemOption[]} Options - An Array of type definitions for each allowed extended type. The first item in the array should
  *     be the default option.
- * @param {boolean} IsClothes - Whether or not the click is performed on a clothing item.
  * @returns {void} Nothing
  */
-function ExtendedItemClick(Options, IsClothes) {
+function ExtendedItemClick(Options) {
 
 	// Exit button
 	if (MouseIn(1885, 25, 90, 85)) {
@@ -116,9 +115,9 @@ function ExtendedItemClick(Options, IsClothes) {
 
 	var IsSelfBondage = CharacterGetCurrent().ID === 0;
 	if (Options.length === 2) {
-		ExtendedItemClickTwo(Options, IsSelfBondage, IsClothes);
+		ExtendedItemClickTwo(Options, IsSelfBondage);
 	} else {
-		ExtendedItemClickGrid(Options, IsSelfBondage, IsClothes);
+		ExtendedItemClickGrid(Options, IsSelfBondage);
 	}
 }
 
@@ -127,10 +126,9 @@ function ExtendedItemClick(Options, IsClothes) {
  * @param {ExtendedItemOption[]} Options - An Array of type definitions for each allowed extended type. The first item in the array should
  *     be the default option.
  * @param {ExtendedItemOption} Option - The selected type definition
- * @param {boolean} IsClothes - Whether or not the click is performed on a clothing item.
  * @returns {void} Nothing
  */
-function ExtendedItemSetType(Options, Option, IsClothes) {
+function ExtendedItemSetType(Options, Option) {
 	var C = CharacterGetCurrent();
 	var FunctionPrefix = ExtendedItemFunctionPrefix();
 
@@ -164,24 +162,21 @@ function ExtendedItemSetType(Options, Option, IsClothes) {
 	}
 
 	DialogFocusItem.Property = NewProperty;
-	CharacterRefresh(C, !IsClothes); // Does not sync appearance while in the wardrobe
-	
-	// For a restraint, we might publish an action or change the dialog of a NPC
-	if (!IsClothes) {
-		ChatRoomCharacterUpdate(C);
-		if (CurrentScreen === "ChatRoom") {
-			// If we're in a chatroom, call the item's publish function to publish a message to the chatroom
-			CommonCallFunctionByName(FunctionPrefix + "PublishAction", C, Option, PreviousOption);
+	CharacterRefresh(C);
+	ChatRoomCharacterUpdate(C);
+
+	if (CurrentScreen === "ChatRoom") {
+		// If we're in a chatroom, call the item's publish function to publish a message to the chatroom
+		CommonCallFunctionByName(FunctionPrefix + "PublishAction", C, Option, PreviousOption);
+	} else {
+		DialogFocusItem = null;
+		if (C.ID === 0) {
+			// Player is using the item on herself
+			DialogMenuButtonBuild(C);
 		} else {
-			DialogFocusItem = null;
-			if (C.ID === 0) {
-				// Player is using the item on herself
-				DialogMenuButtonBuild(C);
-			} else {
-				// Otherwise, call the item's NPC dialog function, if one exists
-				CommonCallFunctionByName(FunctionPrefix + "NpcDialog", C, Option, PreviousOption);
-				C.FocusGroup = null;
-			}
+			// Otherwise, call the item's NPC dialog function, if one exists
+			CommonCallFunctionByName(FunctionPrefix + "NpcDialog", C, Option, PreviousOption);
+			C.FocusGroup = null;
 		}
 	}
 }
@@ -242,16 +237,15 @@ function ExtendedItemDrawGrid(Options, DialogPrefix, IsSelfBondage) {
  * @param {ExtendedItemOption[]} Options - An Array of type definitions for each allowed extended type. The first item in the array should
  *     be the default option.
  * @param {boolean} IsSelfBondage - Whether or not the player is applying the item to themselves
- * @param {boolean} IsClothes - Whether or not the click is performed on a clothing item.
  * @returns {void} Nothing
  */
-function ExtendedItemClickTwo(Options, IsSelfBondage, IsClothes) {
+function ExtendedItemClickTwo(Options, IsSelfBondage) {
 	for (let I = 0; I < Options.length; I++) {
 		var X = 1175 + I * 425;
 		var Y = 550;
 		var Option = Options[I];
 		if (MouseIn(X, Y, 225, 225) && DialogFocusItem.Property.Type !== Option.Property.Type) {
-			ExtendedItemHandleOptionClick(Options, Option, IsSelfBondage, IsClothes);
+			ExtendedItemHandleOptionClick(Options, Option, IsSelfBondage);
 		}
 	}
 }
@@ -261,10 +255,9 @@ function ExtendedItemClickTwo(Options, IsSelfBondage, IsClothes) {
  * @param {ExtendedItemOption[]} Options - An Array of type definitions for each allowed extended type. The first item in the array should
  *     be the default option.
  * @param {boolean} IsSelfBondage - Whether or not the player is applying the item to themselves
- * @param {boolean} IsClothes - Whether or not the click is performed on a clothing item.
  * @returns {void} Nothing
  */
-function ExtendedItemClickGrid(Options, IsSelfBondage, IsClothes) {
+function ExtendedItemClickGrid(Options, IsSelfBondage) {
 
 	// Pagination button
 	if ((Options.length > 4) && MouseIn(1775, 25, 90, 85))
@@ -278,7 +271,7 @@ function ExtendedItemClickGrid(Options, IsSelfBondage, IsClothes) {
 		var Y = 450 + (Math.floor(offset / 2) * 300);
 		var Option = Options[I];
 		if (MouseIn(X, Y, 225, 225) && DialogFocusItem.Property.Type !== Option.Property.Type) {
-			ExtendedItemHandleOptionClick(Options, Option, IsSelfBondage, IsClothes);
+			ExtendedItemHandleOptionClick(Options, Option, IsSelfBondage);
 		}
 	}
 }
@@ -289,15 +282,14 @@ function ExtendedItemClickGrid(Options, IsSelfBondage, IsClothes) {
  *     be the default option.
  * @param {ExtendedItemOption} Option - The selected type definition
  * @param {boolean} IsSelfBondage - Whether or not the player is applying the item to themselves
- * @param {boolean} IsClothes - Whether or not the click is performed on a clothing item.
  * @returns {void} Nothing
  */
-function ExtendedItemHandleOptionClick(Options, Option, IsSelfBondage, IsClothes) {
+function ExtendedItemHandleOptionClick(Options, Option, IsSelfBondage) {
 	var requirementMessage = ExtendedItemRequirementCheckMessage(Option, IsSelfBondage);
 	if (requirementMessage) {
 		DialogExtendedMessage = requirementMessage;
 	} else {
-		ExtendedItemSetType(Options, Option, IsClothes);
+		ExtendedItemSetType(Options, Option);
 	}
 }
 
