51eab9f5a91519b5a43511d4238750d264930b9f
Ada <gubertpol0@gmail.com>
2020-10-06 13:58:03 -0700

    Added blindfold version with configurable blindness

---
+++ BondageClub/Screens/Inventory/ItemHead/InteractiveVisor/InteractiveVisor.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var InventoryItemHeadInteractiveVisorOptions = [
+	{
+		Name: "Transparent",
+		Property: {
+			Type: null,
+		},
+	},
+	{
+		Name: "Blind",
+		Property: {
+			Type: "Blind",
+			Effect: ["BlindNormal", "Prone"],
+		},
+	},
+];
+
+function InventoryItemHeadInteractiveVisorLoad() {
+	ExtendedItemLoad(InventoryItemHeadInteractiveVisorOptions, "SelectVisorType");
+}
+
+function InventoryItemHeadInteractiveVisorDraw() {
+	ExtendedItemDraw(InventoryItemHeadInteractiveVisorOptions, "InteractiveVisorHeadType");
+}
+
+function InventoryItemHeadInteractiveVisorClick() {
+	ExtendedItemClick(InventoryItemHeadInteractiveVisorOptions);
+}
+
+function InventoryItemHeadInteractiveVisorPublishAction(C, Option) {
+	var Message = "InteractiveVisorHeadSet" + Option.Name;
+	var Dictionary = [
+		{ Tag: "SourceCharacter", Text: Player.Name, MemberNumber: Player.MemberNumber },
+		{ Tag: "DestinationCharacter", Text: C.Name, MemberNumber: C.MemberNumber },
+		{ Tag: "TargetCharacter", Text: C.Name, MemberNumber: C.MemberNumber },
+	];
+	ChatRoomPublishCustomAction(Message, true, Dictionary);
+}
+
+
+
+function InventoryItemHeadInteractiveVisorValidate(C, Option) {
+	var Allowed = true;
+
+	if (DialogFocusItem.Property.LockedBy && !DialogCanUnlock(C, DialogFocusItem)) {
+		DialogExtendedMessage = DialogFind(Player, "CantChangeWhileLockedFuturisticVisor");
+		Allowed = false;
+	} 
+
+	return Allowed;
+}
+
+function InventoryItemHeadInteractiveVisorNpcDialog(C, Option) {
+	C.CurrentDialog = DialogFind(C, "ItemHeadInteractiveVisor" + Option.Name, "ItemHead");
+}
