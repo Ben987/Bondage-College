6448d4edda349e2200cd8f3e6eb40187330302cd
Ada <gubertpol0@gmail.com>
2020-10-19 16:08:55 -0700

    Futuristic Collar
    Added a futuristic collar, which has the functionality of allowing a player to quickly lock all items with "Futuristic" or "Interactive" in their name on a player. It only allows players to use the following locks, and only allows the player to lock or unlock the items if they own the locks and could normally lock the player:
     - Metal Padlock
     - Exclusive Padlock
     - 5 Min Padlock
     - Mistress Padlock
     - Lovers Padlock
     - Owner Padlock
    
    
    This has a dependency on the futuristic item revamp

--- BondageClub/Assets/Female3DCG/Female3DCG.js
+++ BondageClub/Assets/Female3DCG/Female3DCG.js
@@ -2105,6 +2105,13 @@ var AssetFemale3DCG = [
 					{ Name: "Rings" }
 				]
 			},
+			{
+				Name: "FuturisticCollar", Fetish: ["Metal"], Value: 100, Difficulty: 50, Time: 12, DefaultColor: ["#40812C", "Default"], AllowLock: true, Extended: true,
+				Layer: [
+					{ Name: "Display" },
+					{ Name: "Band" },
+				]
+			},
 			{ Name: "LeatherChoker", Fetish: ["Leather"], Value: 10, Difficulty: 50, Time: 5, AllowLock: true },
 			{ Name: "PetCollar", Fetish: ["Leather", "Pet"], Value: -1, Difficulty: 50, Time: 5, AllowLock: true },
 			{ Name: "MaidCollar", Fetish: ["Lingerie"], Value: 30, Difficulty: 50, Time: 5, AllowLock: true },
