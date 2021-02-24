249564e7ae98a2a8e78506002978cd5475084900
Ada <gubertpol0@gmail.com>
2021-02-23 16:53:01 -0800

    Mazegen

--- BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonGame.js
+++ BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonGame.js
@@ -8,6 +8,11 @@ var KinkyDungeonMapIndex = [];
 var KinkyDungeonGrid = ""
 var KinkyDungeonGrid_Last = ""
 var KinkyDungeonGridSize = 72
+var KinkyDungeonGridWidth = 19
+var KinkyDungeonGridHeight = 11
+
+
+var KinkyDungeonSpriteSize = 72
 
 var KinkyDungeonCanvas = document.createElement("canvas");
 var KinkyDungeonContext = null
@@ -35,12 +40,19 @@ function KinkyDungeonInitialize(Level, Random) {
 	
 	
 	KinkyDungeonContext = KinkyDungeonCanvas.getContext("2d")
-	KinkyDungeonCanvas.width = KinkyDungeonGridSize*20;
-	KinkyDungeonCanvas.height = KinkyDungeonGridSize*10;
+	KinkyDungeonCanvas.width = KinkyDungeonGridSize*KinkyDungeonGridWidth;
+	KinkyDungeonCanvas.height = KinkyDungeonGridSize*KinkyDungeonGridHeight;
 }
 // Starts the the game at a specified level
 function KinkyDungeonCreateMap(MapParams) {
-	KinkyDungeonGrid = "11111111111111111111\n11111111111111111111\n11111111111111111111\n11111111111111111111\n11111111111111111111\n11111111111111111111\n11111111111111111111\n11111111111111111111\n11111111111111111111\n11111111111111111111"
+	KinkyDungeonGrid = ""
+	
+	for (let X = 0; X < KinkyDungeonGridHeight; X++) {
+		for (let Y = 0; Y < KinkyDungeonGridWidth; Y++)
+			KinkyDungeonGrid = KinkyDungeonGrid + '1'
+		KinkyDungeonGrid = KinkyDungeonGrid + '\n'
+	}
+	
 	KinkyDungeonGrid_Last = ""
 	
 	
@@ -48,26 +60,34 @@ function KinkyDungeonCreateMap(MapParams) {
 	var rows = KinkyDungeonGrid.split('\n')
 	
 	
-	var height = rows.length
-	var width = rows[0].length
+	var height = KinkyDungeonGridHeight
+	var width = KinkyDungeonGridWidth
 	
-	var startpos = 1 + Math.floor(Math.random() * (height - 2))
-	var endpos = 1 + Math.floor(Math.random() * (height - 2))
+	var startpos = 1 + 2*Math.floor(Math.random()*0.5 * (height - 2))
+	var endpos = 1 + 2*Math.floor(Math.random()*0.5 * (height - 2))
 	
-	KinkyDungeonMapSet(1, startpos, '0')
-	KinkyDungeonMapSet(rows[0].length-2, endpos, '0')
 	
-	// Make rooms
-	var rooms = []
+	
+	
+	// MAP GENERATION
+	
+	var VisitedRooms = []
+	KinkyDungeonMapSet(1, startpos, '0', VisitedRooms)
+	//KinkyDungeonMapSet(rows[0].length-2, endpos, '0')
+	
+	// Use primm algorithm
+	KinkyDungeonCreateMaze(VisitedRooms, width, height)
+	
+	// Add rooms
+	/*var rooms = []
 	var openness = MapParams["openness"]
 	var tunnellength = MapParams["tunnellength"]
 	var roomcount = 50
 	
-	
 	for (let R = 0; R < roomcount; R++) {
 		var size = 1+Math.floor(Math.random() * (openness))
-		var x = 1 + Math.floor(Math.random() * (width - 1 - size))
-		var y = 1 + Math.floor(Math.random() * (height - 1 - size))
+		var x = 1 + 2*Math.floor(Math.random()*0.5 * (width - 1 - size))
+		var y = 1 + 2*Math.floor(Math.random()*0.5 * (height - 1 - size))
 		var occupied = false;
 		
 		
@@ -92,18 +112,88 @@ function KinkyDungeonCreateMap(MapParams) {
 		for (let X = 0; X < room.size; X++)
 			for (let Y = 0; Y < room.size; Y++)
 				KinkyDungeonMapSet(room.x+X, room.y+Y, '0')
-	}
+
+	}*/
+	
 	
 }
 
+function KinkyDungeonCreateMaze(VisitedRooms, width, height) {
+	var Walls = {}
+	var WallsList = {}
+	var VisitedCells = {}
+	
+	VisitedCells[VisitedRooms[0].x + "," + VisitedRooms[0].y] = {x:VisitedRooms[0].x, y:VisitedRooms[0].y}
+	
+	// Walls are basically even/odd pairs
+	for (let X = 2; X < width; X += 2)
+		for (let Y = 1; Y < height; Y += 2)
+			if (KinkyDungeonMapGet(X, Y) == '1') {
+				Walls[X + "," + Y] = {x:X, y:Y}
+			}
+	for (let X = 1; X < width; X += 2)
+		for (let Y = 2; Y < height; Y += 2)
+			if (KinkyDungeonMapGet(X, Y) == '1') {
+				Walls[X + "," + Y] = {x:X, y:Y}
+			}
+			
+	KinkyDungeonMazeWalls(VisitedRooms[0], Walls, WallsList)
+	
+	var WallKeys = Object.keys(WallsList)
+	var CellKeys = Object.keys(VisitedCells)
+			
+	while (WallKeys.length > 0) {
+		var I = Math.floor(Math.random() * WallKeys.length)
+		var wall = Walls[WallKeys[I]]
+		var unvisitedCell = null
+		if (wall.x % 2 == 0) { //horizontal wall
+			if (!VisitedCells[(wall.x-1) + "," + wall.y]) unvisitedCell = {x:wall.x-1, y:wall.y}
+			if (!VisitedCells[(wall.x+1) + "," + wall.y]) {
+				if (unvisitedCell) unvisitedCell = null
+				else unvisitedCell = {x:wall.x+1, y:wall.y}
+			}
+		} else { //vertical wall
+			if (!VisitedCells[wall.x + "," + (wall.y-1)]) unvisitedCell = {x:wall.x, y:wall.y-1}
+			if (!VisitedCells[wall.x + "," + (wall.y+1)]) {
+				if (unvisitedCell) unvisitedCell = null
+				else unvisitedCell = {x:wall.x, y:wall.y+1}
+			}
+		}
+		
+		if (unvisitedCell) {
+			delete Walls[wall.x + "," + wall.y]
 
+			KinkyDungeonMapSet(wall.x, wall.y, '0')
+			KinkyDungeonMapSet(unvisitedCell.x, unvisitedCell.y, '0')
+			VisitedCells[unvisitedCell.x + "," + unvisitedCell.y] = unvisitedCell
+			
+			KinkyDungeonMazeWalls(unvisitedCell, Walls, WallsList)
+		}
 
-function KinkyDungeonMapSet(X, Y, SetTo) {
-	var height = KinkyDungeonGrid.split('\n').length
-	var width = KinkyDungeonGrid.split('\n')[0].length
+		delete WallsList[wall.x + "," + wall.y]
+		
+		
+		WallKeys = Object.keys(WallsList)
+		CellKeys = Object.keys(VisitedCells)
+	}
+
+}
+
+function KinkyDungeonMazeWalls(Cell, Walls, WallsList) {
+	if (Walls[(Cell.x+1) + "," + Cell.y]) WallsList[(Cell.x+1) + "," + Cell.y] = {x:Cell.x+1, y:Cell.y}
+	if (Walls[(Cell.x-1) + "," + Cell.y]) WallsList[(Cell.x-1) + "," + Cell.y] = {x:Cell.x-1, y:Cell.y}
+	if (Walls[Cell.x + "," + (Cell.y+1)]) WallsList[Cell.x + "," + (Cell.y+1)] = {x:Cell.x, y:Cell.y+1}
+	if (Walls[Cell.x + "," + (Cell.y-1)]) WallsList[Cell.x + "," + (Cell.y-1)] = {x:Cell.x, y:Cell.y-1}
+}
+
+function KinkyDungeonMapSet(X, Y, SetTo, VisitedRooms) {
+	var height = KinkyDungeonGridHeight
+	var width = KinkyDungeonGridWidth
 	
 	if (X > 0 && X < width-1 && Y > 0 && Y < height-1) {
 		KinkyDungeonGrid = KinkyDungeonGrid.replaceAt(X + Y*(width+1), SetTo)
+		if (VisitedRooms)
+			VisitedRooms.push({x: X, y: Y})
 		return true;
 	}
 	return false;
@@ -136,8 +226,8 @@ function KinkyDungeonDrawGame() {
 			for (let R = 0; R < rows.length; R++)  {
 				for (let X = 0; X < rows[R].length; X++)  {
 					if (rows[R][X] == "1")
-						DrawImageZoomCanvas("Screens/Minigame/KinkyDungeon/Wall" + KinkyDungeonMapIndex[MiniGameKinkyDungeonCheckpoint] + ".png", KinkyDungeonContext, 0, 0, KinkyDungeonGridSize, KinkyDungeonGridSize, X*KinkyDungeonGridSize, R*KinkyDungeonGridSize, KinkyDungeonGridSize, KinkyDungeonGridSize, false)
-					else DrawImageZoomCanvas("Screens/Minigame/KinkyDungeon/Floor" + KinkyDungeonMapIndex[MiniGameKinkyDungeonCheckpoint] + ".png", KinkyDungeonContext, 0, 0, KinkyDungeonGridSize, KinkyDungeonGridSize, X*KinkyDungeonGridSize, R*KinkyDungeonGridSize, KinkyDungeonGridSize, KinkyDungeonGridSize, false)
+						DrawImageZoomCanvas("Screens/Minigame/KinkyDungeon/Wall" + KinkyDungeonMapIndex[MiniGameKinkyDungeonCheckpoint] + ".png", KinkyDungeonContext, 0, 0, KinkyDungeonSpriteSize, KinkyDungeonSpriteSize, X*KinkyDungeonGridSize, R*KinkyDungeonGridSize, KinkyDungeonGridSize, KinkyDungeonGridSize, false)
+					else DrawImageZoomCanvas("Screens/Minigame/KinkyDungeon/Floor" + KinkyDungeonMapIndex[MiniGameKinkyDungeonCheckpoint] + ".png", KinkyDungeonContext, 0, 0, KinkyDungeonSpriteSize, KinkyDungeonSpriteSize, X*KinkyDungeonGridSize, R*KinkyDungeonGridSize, KinkyDungeonGridSize, KinkyDungeonGridSize, false)
 				}
 			}
 			
