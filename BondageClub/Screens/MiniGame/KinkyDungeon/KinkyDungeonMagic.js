c7c53f017a417c5ff2aa0c8416b87c9bb09098c0
Ada <gubertpol0@gmail.com>
2021-03-01 18:55:15 -0800

    Intro change

---
+++ BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonMagic.js
@@ -0,0 +1,3 @@
+var KinkyDungeonManaCost = 10 // The base mana cost of a spell, multiplied by the spell's level
+var KinkyDungeonRunes = 0 // The number of blank runes the player possesses
+var KinkyDungeonDamageTypes = ["fire", "water", "shock", "cold", "force", "acid"]
