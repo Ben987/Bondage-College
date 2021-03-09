7f97b62652b13993eaa1790afd79e5f9c24cbfff
Ada <gubertpol0@gmail.com>
2021-03-09 00:36:53 -0800

    Balance and fixed enemies not appearing

--- BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonGame.js
+++ BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonGame.js
@@ -509,13 +509,13 @@ function KinkyDungeonClickGame(Level) {
 	
 	// If no buttons are clicked then we handle move
 	else if (KinkyDungeonTargetingSpell) {
-		if (KinkyDungeonSpellValid) {
-			if (MouseIn(canvasOffsetX, canvasOffsetY, KinkyDungeonCanvas.width, KinkyDungeonCanvas.height)) {
+		if (MouseIn(canvasOffsetX, canvasOffsetY, KinkyDungeonCanvas.width, KinkyDungeonCanvas.height)) {
+			if (KinkyDungeonSpellValid) {
 				KinkyDungeonCastSpell(KinkyDungeonTargetX, KinkyDungeonTargetY, KinkyDungeonTargetingSpell)
 				KinkyDungeonAdvanceTime(1)
+				KinkyDungeonTargetingSpell = null
 			}
-			KinkyDungeonTargetingSpell = null
-		}
+		} else KinkyDungeonTargetingSpell = null
 	} else if (MouseIn(canvasOffsetX, canvasOffsetY, KinkyDungeonCanvas.width, KinkyDungeonCanvas.height)) {
 		KinkyDungeonMove(KinkyDungeonMoveDirection)
 	}
@@ -647,7 +647,7 @@ function KinkyDungeonAdvanceTime(delta) {
 			KinkyDungeonState = "End"
 			MiniGameVictory = true
 		}
-		KinkyDungeonCreateMap(KinkyDungeonMapParams[KinkyDungeonMapIndex[MiniGameKinkyDungeonCheckpoint]], KinkyDungeonMapIndex[MiniGameKinkyDungeonCheckpoint])
+		KinkyDungeonCreateMap(KinkyDungeonMapParams[KinkyDungeonMapIndex[MiniGameKinkyDungeonCheckpoint]], MiniGameKinkyDungeonLevel, KinkyDungeonMapIndex[MiniGameKinkyDungeonCheckpoint])
 	}
 	
 	if (KinkyDungeonStatWillpower == 0) {
