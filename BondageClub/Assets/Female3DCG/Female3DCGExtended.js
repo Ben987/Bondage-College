8bea18eb83ecaecef46dcae213c4bb544d52cf58
Ada <gubertpol0@gmail.com>
2021-05-03 14:36:09 -0700

    Kigu mask opacity option

--- BondageClub/Assets/Female3DCG/Female3DCGExtended.js
+++ BondageClub/Assets/Female3DCG/Female3DCGExtended.js
@@ -152,6 +152,22 @@ var AssetFemale3DCGExtended = {
 						Name: "Brows", Key: "br",
 						Options: [{}, {}, {}, {}], // All options are merely cosmetic,
 					},
+					{
+						Name: "Opacity", Key: "op",
+						Options: [
+								{},
+								{
+									Property: {
+										Effect: ["BlindLight"],
+									},
+								},
+								{
+									Property: {
+										Effect: ["BlindHeavy", "Prone"],
+									},
+								}
+							], // Opacity
+					},
 				],
 			},
 		}, // KirugumiMask
