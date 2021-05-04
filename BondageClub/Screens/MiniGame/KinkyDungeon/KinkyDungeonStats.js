4f2f16fd812e111f9678ed24fc1cef604a57ee6d
Ada <gubertpol0@gmail.com>
2021-05-04 16:51:49 -0700

    Balance and prettiness

--- BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonStats.js
+++ BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonStats.js
@@ -65,7 +65,7 @@ var KinkyDungeonBlueKeys = 0;
 var KinkyDungeonNormalBlades = 1;
 var KinkyDungeonEnchantedBlades = 0;
 
-var KinkyDungeonKnifeBreakChance = 0.1;
+var KinkyDungeonKnifeBreakChance = 0.2;
 var KinkyDungeonKeyJamChance = 0.33;
 var KinkyDungeonKeyPickBreakChance = 0.25;
 
