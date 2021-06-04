f5c37682602de1eee16b6141b653b07ddff272ec
Ayesha-678 <59668834+Ayesha-678@users.noreply.github.com>
2021-06-04 16:05:50 -0500

    Item Arms Null

--- BondageClub/Assets/Female3DCG/Female3DCGExtended.js
+++ BondageClub/Assets/Female3DCG/Female3DCGExtended.js
@@ -471,8 +471,8 @@ var AssetFemale3DCGExtended = {
 			Archetype: ExtendedArchetype.TYPED,
 			Config: {
 				Options: [
-					{ Name: "Wrist", Property: { Type: null, SetPose: ["BackBoxTie"] } },
-					{ Name: "Boxtie", Property: { Type: "Boxtie", SetPose: ["BackBoxTie"] } },
+					{ Name: "Wrist", Property: { Type: "Wrist", SetPose: ["BackBoxTie"] } },
+					{ Name: "Boxtie", Property: { Type: null, SetPose: ["BackBoxTie"] } },
 					{ Name: "WristElbow", Property: { Type: "WristElbow", SetPose: ["BackElbowTouch"] } },
 					{ Name: "WristElbowHarness", Property: { Type: "WristElbowHarness", SetPose: ["BackElbowTouch"] } },
 					{ Name: "Hogtie", Property: { Type: "Hogtie", SetPose: ["Hogtied"], Effect: ["Block", "Freeze", "Prone"] } }
