80f05701edd39defb9cdd906a7d11fa23d0b29de
Ada <gubertpol0@gmail.com>
2021-06-10 10:41:06 -0700

    Kinky Dungeon Spectator Fix
    Prevents data from Kinky Dungeon spectating carrying over when playing Kinky Dungeon yourself.

--- BondageClub/Scripts/Dialog.js
+++ BondageClub/Scripts/Dialog.js
@@ -356,6 +356,8 @@ function DialogStartKinkyDungeon() {
 		if (KinkyDungeonPlayerCharacter != Player && CurrentCharacter.MemberNumber) {
 			KinkyDungeonGameData = null;
 			ServerSend("ChatRoomChat", { Content: "RequestFullKinkyDungeonData", Type: "Hidden", Target: CurrentCharacter.MemberNumber });
+		} else {
+			KinkyDungeonGameRunning = false; // Reset the game to prevent carrying over spectator data
 		}
 	}
 	DialogGamingPreviousRoom = CurrentScreen;
