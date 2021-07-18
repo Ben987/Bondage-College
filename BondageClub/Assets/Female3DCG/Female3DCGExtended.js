a92244e887dbe79345ffbdfeb72891a9c12369f5
wildsj <32558920+wildsj@users.noreply.github.com>
2021-07-18 21:41:09 +0100

    Split PetTape into separate arm and leg slots
    Rather than all or nothing, the PetTape can also be applied to just arms or legs and be worn with clothes.

--- BondageClub/Assets/Female3DCG/Female3DCGExtended.js
+++ BondageClub/Assets/Female3DCG/Female3DCGExtended.js
@@ -453,10 +453,10 @@ var AssetFemale3DCGExtended = {
 					{
 						Name: "PetTape",
 						SelfBondageLevel: 10,
-						Prerequisite: ["NoOuterClothes"],
 						Property: {
 							Type: "PetTape",
-							SetPose: ["BackElbowTouch", "Kneel"],
+							SetPose: ["BackElbowTouch"],
+							Block: ["ItemHands"],
 							Difficulty: 7,
 						}
 					},
